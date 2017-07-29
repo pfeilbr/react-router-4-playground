edb4433d5cefb13fc64fa139d474794dff38d3b8
Brian Pfeil <brian.pfeil@gmail.com>
2017-07-29 09:53:45 -0400

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# react-router-4-playground
+
+built with [create-react-app](https://github.com/facebookincubator/create-react-app)
+
+see `src/index.js`
+
+**install deps and run**
+
+```sh
+$ yarn
+$ yarn start
+```
+
+**upgrade all deps**
+```sh
+$ yarn upgrade react react-dom react-redux react-router react-router-dom react-router-redux redux react-scripts
+```
---
+++ src/index.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+//import App from './App'
+import './index.css'
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+const Debug = ({ match, store }) => (
+  <div>
+    <h2>Debug</h2>
+    <p>
+      <pre>
+        <code>
+          { JSON.stringify(match, null, 2) }
+        </code>
+      </pre>
+    </p>
+  </div>
+)
+
+const Root = () => (
+    <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">home</Link>
+        </li>
+        <li>
+          <Link to="/about">about</Link>
+        </li>
+        <li>
+          <Link to="/item/1">item 1</Link>
+        </li>
+        <li>
+          <Link to="/item/2">item 2</Link>
+        </li>
+      </ul>
+      <hr/>
+      <Sidebar />
+      <div style={{float: 'right'}}>
+        <Route exact path="/" component={Debug} />
+        <Route path="/about" component={Debug} />
+        <Route path="/item/:id" component={Debug} />
+      </div>
+      <hr/>
+
+    </div>
+
+    </Router>
+)
+
+const Sidebar = () => (
+  <Router>
+    <div style={{border: 'solid 1px black', float: 'left'}}>
+      <h3>Sidebar</h3>
+      <div>
+        <Route path="/item/:id" component={Debug} />
+      </div>
+    </div>
+  </Router>
+)
+
+ReactDOM.render(
+  <Root />,
+  document.getElementById('root')
+);
