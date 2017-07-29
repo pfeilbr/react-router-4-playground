3280f9bda2be163a8f59a9f8370e680122ce429b
Brian Pfeil <brian.pfeil@gmail.com>
2017-07-29 10:16:29 -0400

    added basic example from react-router site

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 //import App from './App'
+import { BasicExample } from './basic-example'
 import './index.css'
 
 import {
@@ -65,6 +66,6 @@ const Sidebar = () => (
 )
 
 ReactDOM.render(
-  <Root />,
+  <BasicExample /> /* <Root /> */,
   document.getElementById('root')
 );
